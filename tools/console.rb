b5e14619cef686dbf06f847ae064e890385ab407
Miss-Cheese <nat.galligan@gmail.com>
2020-12-11 07:07:57 -0500

    Airbnb exercise done

--- tools/console.rb
+++ tools/console.rb
@@ -4,5 +4,21 @@ def reload
   load 'config/environment.rb'
 end
 
+guest1 = Guest.new("Evan")
+guest2 = Guest.new("Mike")
+guest3 = Guest.new("Roger")
+guest4 = Guest.new("Kevin")
+
+listing1 = Listing.new("Seattle")
+listing2 = Listing.new("Paris")
+listing3 = Listing.new("Amsterdam")
+
+trip1 = Trip.new(guest1, listing3)
+trip2 = Trip.new(guest2, listing1)
+trip3 = Trip.new(guest3, listing3)
+trip4 = Trip.new(guest1, listing2)
+trip5 = Trip.new(guest4, listing1)
+
+
 binding.pry
 0
